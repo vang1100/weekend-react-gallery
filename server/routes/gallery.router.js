70b195e7de75384492db3231965dab940ef8ea51
Padee Vang <vangpadee11@gmail.com>
2024-08-26 18:59:31 -0500

    updated PUT route, will PUT work?

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -4,7 +4,20 @@ const pool = require('../modules/pool.js')
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+  let { id } = req.params;
+  const sqlText = `
+      UPDATE "gallery" SET "likes" = +1
+      WHERE "id" = $1;
+  `;
+  pool.query(sqlText, [id])
+      .then((result) => {
+          console.log(`Got stuff back from the database`, result);
+          res.sendStatus(201);
+      })
+      .catch((error) => {
+          console.log(`Error making database query ${sqlText}`, error);
+          res.sendStatus(500); // Good server always responds
+      })
 });
 
 // GET /gallery
